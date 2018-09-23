2f3a8df7af918354a3435bb1435c8d7b229ae7d5
AlenaShkr <elalshkr@gmail.com>
2018-09-24 00:14:35 +0300

    quadratic-equation: add quick solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  
+  let res=equation.replace('^2', '').split(' * x'); // сделать через регулярные выражения
+
+   let a=+res[0];
+   let b=+res[1].replace(' ', '').replace(' ','');
+   let c=+res[2].replace(' ','').replace(' ','');
+
+   let solution=[];
+   // можно добавить проверку дискрименанта
+   let x1=Math.round((-b+Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a));
+   let x2=Math.round((-b-Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a));
+   solution.push(x1, x2);
+   return solution.sort(function(a,b){return a-b});
 }
