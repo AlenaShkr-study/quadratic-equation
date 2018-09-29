e47d624027a11d02225f76a2f6971c2efd142b96
AlenaShkr <elalshkr@gmail.com>
2018-09-30 02:00:56 +0300

    quadratic-equation: implement solveEquation function by RegEx

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let a=parseInt(equation.match(/\-?\s?\d*\s?\*\s?\w\^\d/gm));
+  let b=equation.match(/\-?\+?\s\d*\s/gm);
+      b=parseInt(b[0].replace(' ',''));
+  let c=+equation.replace(/\-?\s?\d*\s?\*\s?\w\^\d\s\-?\+?\s\d*\s\*\s\w\s/gm, '').replace(' ','');
   
-  let res=equation.replace('^2', '').split(' * x'); // сделать через регулярные выражения
-
-   let a=+res[0];
-   let b=+res[1].replace(' ', '').replace(' ','');
-   let c=+res[2].replace(' ','').replace(' ','');
-
-   let solution=[];
    // можно добавить проверку дискрименанта
-   let x1=Math.round((-b+Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a));
-   let x2=Math.round((-b-Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a));
-   solution.push(x1, x2);
-   return solution.sort(function(a,b){return a-b});
+   const X1=Math.round((-b+Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a));
+   const X2=Math.round((-b-Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a));
+   let solution=[];
+   solution.push(X1, X2);
+
+   return solution.sort((a,b)=> a-b);
 }
